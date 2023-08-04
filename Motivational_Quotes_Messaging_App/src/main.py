68366bb5bcef8b6db5b01f4aeb8f7ef41c13f636
Chiranjeev Veer Srivastava <74554911+GeekGuy-29@users.noreply.github.com>
2023-08-04 22:12:29 +0530

    Core File

---
+++ Motivational_Quotes_Messaging_App/src/main.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+import schedule
+
+def core():
+	
+	url  = "https://www.oberlo.com/blog/motivational-quotes"
+	result = requests.get(url)
+	doc = BeautifulSoup(result.text, "html.parser")
+
+	quotes = doc.find_all("li", style="font-weight: 400;")
+	quote_list = []
+	for i in quotes:
+		quote_list.append(i.text) 
+	n=3
+	del quote_list[len(quote_list) - n:]
+	print(c)
+	requests.post("https://ntfy.sh/ozonenotifapp",
+	data=quote_list[c].encode(encoding='utf-8'))
